662ec927c8f09f07836556ac9bbde4fa69ff81d3
Van Husein <70703161+AkbarHusein@users.noreply.github.com>
2024-09-28 21:13:33 +0700

    feat: :tada: First Commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="container mx-auto">
+    <p class="text-center">&copy; 2022</p>
+  </footer>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <header class="container mx-auto">
+    <h1 class="text-3xl font-bold">Todo App</h1>
+
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+  </header>
+</template>
